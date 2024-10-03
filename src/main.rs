348073a48bdda8988b86e36b8a676ad442c33a90
Thibault <thibault.delrieu.pro@gmail.com>
2024-10-03 21:05:53 +0200

    first file for codingame

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => {
+        $x.trim().parse::<$t>().unwrap()
+    };
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 fn main() {
-    println!("Hello, world!");
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let resources = parse_input!(input_line, i32);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let num_travel_routes = parse_input!(input_line, i32);
+        for i in 0..num_travel_routes as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let building_id_1 = parse_input!(inputs[0], i32);
+            let building_id_2 = parse_input!(inputs[1], i32);
+            let capacity = parse_input!(inputs[2], i32);
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let num_pods = parse_input!(input_line, i32);
+        for i in 0..num_pods as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let pod_properties = input_line.trim_matches('\n').to_string();
+        }
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let num_new_buildings = parse_input!(input_line, i32);
+        for i in 0..num_new_buildings as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let building_properties = input_line.trim_matches('\n').to_string();
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+        println!("TUBE 0 1;TUBE 0 2;POD 42 0 1 0 2 0 1 0 2"); // TUBE | UPGRADE | TELEPORT | POD | DESTROY | WAIT
+    }
 }
